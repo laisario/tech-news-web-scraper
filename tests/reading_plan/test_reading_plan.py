58a580a302f01398d76654510d17c21dff042fe6
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-09-27 16:49:00 -0300

    req 6

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,9 +1,11 @@
 from tech_news.analyzer.reading_plan import (
     ReadingPlanService,
 )  # noqa: F401, E261, E501
-from tech_news.database import db
 import pytest
 from tests.assets.news import NEWS
+
+# Passo 1
+from unittest.mock import Mock, patch
 
 NEW_NOTICE_0 = NEWS[0]
 NEW_NOTICE_1 = NEWS[1]
@@ -13,38 +15,40 @@ NEW_NOTICE_13 = NEWS[13]
 
 def test_reading_plan_group_news():
     reading_plan_service = ReadingPlanService()
-    db.news.delete_many({})
-    db.news.insert_one(NEW_NOTICE_0)
-    db.news.insert_one(NEW_NOTICE_1)
-    db.news.insert_one(NEW_NOTICE_2)
-    db.news.insert_one(NEW_NOTICE_13)
-    expected = (
-        {
-            "readable": [
-                {
-                    "unfilled_time": 3,
-                    "chosen_news": [
-                        (
-                            "noticia_0",
-                            2,
-                        ),
-                        (
-                            "Notícia bacana 1",
-                            4,
-                        ),
-                        (
-                            "Notícia bacana 2",
-                            1,
-                        ),
-                    ],
-                },
-            ],
-            "unreadable": [
-                ("noticia_13", 15)
-            ]
-        }
+
+    mock_db = Mock(
+        return_value=[NEW_NOTICE_0, NEW_NOTICE_1, NEW_NOTICE_2, NEW_NOTICE_13]
     )
-    assert reading_plan_service.group_news_for_available_time(10) == expected
+
+    expected = {
+        "readable": [
+            {
+                "unfilled_time": 3,
+                "chosen_news": [
+                    (
+                        "noticia_0",
+                        2,
+                    ),
+                    (
+                        "Notícia bacana 1",
+                        4,
+                    ),
+                    (
+                        "Notícia bacana 2",
+                        1,
+                    ),
+                ],
+            },
+        ],
+        "unreadable": [("noticia_13", 15)],
+    }
+    with patch(
+        "tech_news.database",
+        mock_db,
+    ):
+        result = reading_plan_service.group_news_for_available_time(10)
+    assert result == expected
+
     with pytest.raises(
         ValueError, match="Valor 'available_time' deve ser maior que zero"
     ):
