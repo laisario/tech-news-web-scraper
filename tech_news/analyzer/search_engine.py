1a89cdf0720d10683ebc731feaa107e96e9842a6
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-09-27 12:26:47 -0300

    req 7, 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,44 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    query = {"title": {"$regex": f"{title}", "$options": "i"}}
+    news = search_news(query)
+    format_news = []
+    if len(news) == 0:
+        return []
+    for new in news:
+        format_news.append((new["title"], new["url"]))
+    return format_news
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime(
+            "%d/%m/%Y"
+        )
+    except ValueError:
+        raise ValueError("Data inválida")
+    query = {"timestamp": formatted_date}
+    news = search_news(query)
+    format_news = []
+    if len(news) == 0:
+        return []
+    for new in news:
+        format_news.append((new["title"], new["url"]))
+    return format_news
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    query = {"category": {"$regex": f"{category}", "$options": "i"}}
+    news = search_news(query)
+    format_news = []
+    if len(news) == 0:
+        return []
+    for new in news:
+        format_news.append((new["title"], new["url"]))
+    return format_news
